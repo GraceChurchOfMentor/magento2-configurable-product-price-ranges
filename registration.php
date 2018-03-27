c1626df1a7784f2d8ea4c94115c730a22e238c7f
Nick Bair <njbair@gmail.com>
2018-03-27 19:26:41 -0400

    localize for GCM

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MyCompany_ConfigurableProductPriceRanges',
+    'GraceChurchMentor_ConfigurableProductPriceRanges',
     __DIR__
 );
 ?>
